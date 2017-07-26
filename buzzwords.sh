91cf02172e6c1bc02f9279ece7b5c3a3035bdd42
izcet <isaacjrhett@gmail.com>
2017-07-26 14:01:02 -0700

    basic prototype done

--- buzzwords.sh
+++ buzzwords.sh
@@ -1,2 +1,30 @@
 #!/bin/sh
 
+SRCS=txts
+OPEN="1_opener.txt"
+PLAT="2_platform.txt"
+TRAN="3_transition.txt"
+VERB="4_action.txt"
+SUBJ="5_subject.txt"
+
+UNO=$SRCS/$OPEN
+DOS=$SRCS/$PLAT
+TRE=$SRCS/$TRAN
+QUA=$SRCS/$VERB
+CIN=$SRCS/$SUBJ
+
+function getLine () {
+	DOC=$1
+	LEN=$(wc -l $DOC | sed 's/ *//' | cut -d' ' -f1)
+	RND=$(($(($RANDOM % $LEN)) + 1))
+	TXT=$(head -n $RND $DOC | tail -1)
+	echo $TXT
+}
+
+UNO=$(getLine $UNO)
+DOS=$(getLine $DOS)
+TRE=$(getLine $TRE)
+QUA=$(getLine $QUA)
+CIN=$(getLine $CIN)
+
+echo "$UNO $DOS $TRE $QUA $CIN."
